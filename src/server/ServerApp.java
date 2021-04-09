21ad694225cdce1d9e3cd6c4662614fc5ef21658
Santo-G <santo-g@users.noreply.github.com>
2021-04-09 08:17:56 +0200

    chore: updated ServerApp with model component linking

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -1,4 +1,31 @@
 package server;
 
-public class ServerApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import server.model.ServerModel;
+
+public class ServerApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/ServerView.fxml"));
+        Parent root = (Parent) loader.load();
+        stage.setTitle("Mail Server");
+        Image icon= new Image("./utils/server-icon.png");
+        stage.getIcons().add(icon);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("View/ServerView.css").toExternalForm());
+        stage.show();
+        ServerModel model = new ServerModel();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
